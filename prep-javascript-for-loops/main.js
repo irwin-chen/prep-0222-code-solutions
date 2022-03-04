40c27d8557416bbd9cc0a0c195aba4f1f9537cfd
Irwin Chen <irwin.t.chen@gmail.com>
2022-03-04 00:33:19 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+// create your loops here.
+
+for (var i = 0; i <= 9; i++) {
+  console.log(i);
+}
+
+for (var i = 0; i <= 9; i++) {
+  console.log(2 * i);
+}
+
+for (var i = 100; i > 0; i--) {
+  console.log('Time till explosion: ' + i + '!');
+}
